f972ee93509ec071526ff2d2c2cf21dc2b41bdb1
Storu Fujimori <satoru.fujimori@gmail.com>
2017-11-06 16:07:35 +0900

    Add TableStorageSample

---
+++ TableStorageSample/TableStorageSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TableStorageSample
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
